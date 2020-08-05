ca500a2db85031b8a4fa8984798f26dd6a03dc47
Marcos Wellington <marcoswoc@gmail.com>
2020-08-04 23:20:03 -0300

    Adicionando entidades

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        private IList<Subscription> _subscriptions;
+        public Student(string firstName, string lastName, string document, string email, string address)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Document = document;
+            this.Email = email;
+            this.Address = address;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            foreach (var sub in Subscriptions)
+                sub.Inactivate();
+
+            _subscriptions.Add(subscription);
+        }
+    }
+}
