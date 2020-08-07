f06f81d3550bb262357953dea02ae0632de057ae
Marcos Wellington <marcoswoc@gmail.com>
2020-08-06 23:45:55 -0300

    Criando os Commands

---
+++ PaymentContext.Domain/Commands/CreateCreditCardSubscriptionCommand.cs
@@ -0,0 +1,46 @@
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateCreditCardSubscriptionCommand : Notifiable, ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; } 
+        public string Email { get; set; }
+       
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionNumber { get; set; }
+        public string PaymentNumberNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }     
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get;  set; }
+        public string Payer { get; set; }
+        public string PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string PayerEmail { get; set; }
+        
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                    .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+            );
+        }
+    }
+}
